76f2cebed2d5af593ec76b12392c9cc6cdfe017c
Caen De Silva <caen@desilva.se>
2022-12-09 21:22:16 +0100

    Update index.blade.php

--- index.blade.php
+++ index.blade.php
@@ -1,9 +1,38 @@
 <x-hyde::ui.layouts.focus>
-    <x-hyde::ui.components.heading>
-        Heading: Hello World!
+    <x-hyde::ui.components.heading align="left">
+        Heading Component
     </x-hyde::ui.components.heading>
 
+    <div class="mt-3">
+        <x-hyde::ui.components.prose>
+            <p>The above heading component is normally centered but is aligned here.</p>
+        </x-hyde::ui.components.prose>
+    </div>
+
     <div class="mt-8">
+        <x-hyde::ui.components.prose>
+            <h3>Prose Component</h3>
+            <p>Any text-related <abbr>HTML</abbr> here will be styled using the <a href="https://tailwindcss.com/docs/typography-plugin" rel="nofollow noopener" target="_blank">Tailwind Typography</a> plugin.</p>
+        </x-hyde::ui.components.prose>
+    </div>
+
+    <div class="mt-8">
+        <x-hyde::ui.components.prose>
+            <x-hyde::ui.components.markdown>
+                ### Markdown Component
+
+                Any **Markdown** within these tags will be *compiled* to HTML.<br>
+                _Perfect_ to use with the `Prose` component so that it gets styled!
+            </x-hyde::ui.components.markdown>
+        </x-hyde::ui.components.prose>
+    </div>
+
+    <div class="mt-8">
+        <x-hyde::ui.components.prose class="mb-4">
+            <h3>Buttons & Inputs</h3>
+            <p>These stylish and simple buttons are perfect for all your buttoning needs!</p>
+        </x-hyde::ui.components.prose>
+
         <x-hyde::ui.components.button-primary>
             Primary Button
         </x-hyde::ui.components.button-primary>
@@ -14,10 +43,19 @@
     </div>
 
     <div class="mt-3">
-        <x-hyde::ui.components.input value="Input" />
+        <x-hyde::ui.components.prose class="mb-3">
+            <p>The kit even comes with a simple input element too!</p>
+        </x-hyde::ui.components.prose>
+        <x-hyde::ui.components.input placeholder="Write something!" />
     </div>
 
-    <div>
+    <div class="mt-8 sm:-mx-6">
+        <x-hyde::ui.components.prose class="mb-2 sm:px-6">
+            <h3>Cards</h3>
+            <p>The versatile card component lets you create perfect cards for your site.<br>
+                Of course it comes with a variety of options!</p>
+        </x-hyde::ui.components.prose>
+
         <x-hyde::ui.components.card>
             A card with some content.
         </x-hyde::ui.components.card>
@@ -30,7 +68,6 @@
             A card with some content.
         </x-hyde::ui.components.card>
 
-
         <x-hyde::ui.components.card>
             <x-slot name="title">
                 Card Title & Footer
@@ -38,30 +75,9 @@
 
             A card with some content and a footer.
 
-            <x-slot name="footer" class="text-center">
-                <x-hyde::ui.components.button-primary>
-                    Primary Button
-                </x-hyde::ui.components.button-primary>
+            <x-slot name="footer">
+                Footer content
             </x-slot>
         </x-hyde::ui.components.card>
-    </div>
-
-    <div class="mt-8">
-        <x-hyde::ui.components.prose>
-            <h2>Prose Heading</h2>
-            <p>Prose paragraph</p>
-        </x-hyde::ui.components.prose>
-    </div>
-
-    <div class="mt-8">
-        <x-hyde::ui.components.prose>
-            <x-hyde::ui.components.markdown>
-### Markdown
-
-Remember to de-indent the Markdown content otherwise it will be rendered as a code block.
-
-You may also want to wrap this in a `class="prose"` element or the Markdown will not be styled.
-            </x-hyde::ui.components.markdown>
-        </x-hyde::ui.components.prose>
     </div>
 </x-hyde::ui.layouts.focus>
